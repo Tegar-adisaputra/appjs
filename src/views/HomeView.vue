67eb201118934ce2b745defcb4b75f932cfd7938
Tegar Adisaputra <tegara25@gmail.com>
2022-12-07 17:26:59 +0700

    VuejsApp

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <NavbarItem />
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarItem from "@/components/NavbarItem.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    NavbarItem,
+  },
+};
+</script>
