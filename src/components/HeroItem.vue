143e3eb3343afedf475b94024a88908d198bf25c
Tegar Adisaputra <tegara25@gmail.com>
2022-12-12 13:57:08 +0700

    mounting API

--- src/components/HeroItem.vue
+++ src/components/HeroItem.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="hero">
-    <!-- dekstop -->
-    <div class="d-none d-md-block">
-      <div class="row mt-3">
-        <div class="col-md-6">
-          <div class="d-flex h-100">
-            <div class="justify-content-center align-self-center">
-              <h2>
-                <strong>Delicious Food Menu,</strong> <br />
-                In your gadget
-              </h2>
-              <p>Order NOW!</p>
-              <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>&nbsp; Order</button>
+  <div class="container">
+    <div class="hero">
+      <!-- dekstop -->
+      <div class="d-none d-md-block">
+        <div class="row mt-3">
+          <div class="col-md-6">
+            <div class="d-flex h-100">
+              <div class="justify-content-center align-self-center konten-hero">
+                <h2>
+                  <strong>Delicious Food Menu,</strong> <br />
+                  In your gadget
+                </h2>
+                <p>Order NOW!</p>
+                <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>&nbsp; Order</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-6">
-          <img src="../assets/img/hero.png" width="400px" />
+          <div class="col-md-6">
+            <img src="../assets/img/hero.png" width="400px" />
+          </div>
         </div>
       </div>
-    </div>
-    <!-- mobile -->
-    <div class="d-md-block d-md-none">
-      <div class="row mt-3 d-sm-block d-md-none">
-        <div class="col-md-6 text-center">
-          <img src="../assets/img/hero.png" width="60%" />
-        </div>
-        <div class="col-md-6 mt-3">
-          <div class="d-flex h-100">
-            <div class="justify-content-center align-self-center">
-              <h2>
-                <strong>Delicious Food Menu,</strong> <br />
-                In your gadget
-              </h2>
-              <p>Order NOW!</p>
-              <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>&nbsp; Order</button>
+      <!-- mobile -->
+      <div class="d-md-block d-md-none">
+        <div class="row mt-3 d-sm-block d-md-none">
+          <div class="col-md-6 text-center">
+            <img src="../assets/img/hero.png" width="60%" />
+          </div>
+          <div class="col-md-6 mt-3">
+            <div class="d-flex h-100">
+              <div class="justify-content-center align-self-center">
+                <h2>
+                  <strong>Delicious Food Menu,</strong> <br />
+                  In your gadget
+                </h2>
+                <p>Order NOW!</p>
+                <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>&nbsp; Order</button>
+              </div>
             </div>
           </div>
         </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,19 @@
     <NavbarItem />
     <div class="container">
       <HeroItem />
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <h2>Best Seller</h2>
+        </div>
+        <div class="col-md-6">
+          <router-link to="/foods" class="btn btn-success float-right"><b-icon-eye></b-icon-eye> See All </router-link>
+        </div>
+      </div>
+      <div class="row mb-3">
+        <div class="col-md-3 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,12 +24,37 @@
 // @ is an alias to /src
 import NavbarItem from "@/components/NavbarItem.vue";
 import HeroItem from "@/components/HeroItem.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
   components: {
     NavbarItem,
     HeroItem,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProduct(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then(function (response) {
+        // handle success
+        this.setProduct(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("Gagal : ", error);
+      });
   },
 };
 </script>
